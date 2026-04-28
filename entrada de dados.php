b3c689b038870053e295b60fc4a805654316da03
joaorosario-oss <joao.rosario@edu.senai.br>
2026-04-28 12:08:40 -0300

    Add files via upload

--- entrada de dados.php
+++ entrada de dados.php
@@ -1,21 +1,25 @@
 <?php
 require_once "bibliotecaFuncoes"
+
 use function conversao/dolarParaReal;
-
-use function conversao/euroParaReal;
-
-use function conversao/pesoParaReal;
 
 use function conversao/libraParaReaL;
 
 use function conversao/ieneParaRea;
 
+use function conversao/euroParaReal;
 
+use function conversao/pesoParaReal;
 
+use function saude/calcularImc;
 
+use function saude/valorIdealAgua;
 
+use function saude/frquenciaCardiacaMaxima;
 
+use function saude/converterLibrasParaQuila;
 
+use function saude/calcularCaloriasBasais;
 
 
 >?
