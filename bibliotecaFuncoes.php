60ca0cdbd414103ac8293bc04cfb37bf8dddce04
joaorosario-oss <joao.rosario@edu.senai.br>
2026-04-28 10:31:25 -0300

    Add files via upload

---
+++ bibliotecaFuncoes.php
@@ -0,0 +1,47 @@
+<?php
+require_once "bibliotecaFuncoes"
+namespace conversao {
+// atividade 01 
+// echo "status:", conversão (valor=5,cotação=5)
+Function dolarParaReal($valor, $cotacao)
+return $valor*$cotacao
+$valor=5;
+$cotacao=5;
+
+
+Function euroParaReal($valor, $cotacao)
+return $valor/$cotacao
+$valor=5;
+$cotacao=5,85;
+
+Function pesoParaReal($valor, $cotacao)
+return $valor*$cotacao
+$valor=5;
+$cotacao=0,0035;
+
+Function libraParaReal($valor, $cotacao)
+return $valor*$cotacao
+$valor=5;
+$cotacao=6,75;
+
+Function ieneParaReal($valor, $cotacao)
+return $valor*$cotacao
+$valor=5;
+$cotacao=31,88;
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ entrada de dados.php
@@ -0,0 +1,21 @@
+<?php
+require_once "bibliotecaFuncoes"
+use function conversao/dolarParaReal;
+
+use function conversao/euroParaReal;
+
+use function conversao/pesoParaReal;
+
+use function conversao/libraParaReaL;
+
+use function conversao/ieneParaRea;
+
+
+
+
+
+
+
+
+
+>?
